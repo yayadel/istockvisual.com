6f0d78802df4f3ff015b52c73ab8ddcb1b11b286
yayadel <yayadel@users.noreply.github.com>
2026-08-13 14:01:10 +0800

    auto backup (cursor): 2026-08-13 14:01:09

--- src/components/ImageEditor.tsx
+++ src/components/ImageEditor.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState, type CSSProperties } from 'react';
 import { createPortal } from 'react-dom';
 import { isFreeDownloadSize } from '../lib/download-sizes';
 import {
