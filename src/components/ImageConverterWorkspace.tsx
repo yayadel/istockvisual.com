201f5af035668eb65c9eab1e560e3cb2d96f6f26
yayadel <yayadel@users.noreply.github.com>
2026-08-13 17:12:40 +0800

    auto backup (cursor): 2026-08-13 17:12:38

--- src/components/ImageConverterWorkspace.tsx
+++ src/components/ImageConverterWorkspace.tsx
@@ -10,6 +10,7 @@ import {
 	type ConvertItem,
 	type ConvertSettings,
 } from '../lib/image-convert';
+import { ToolsDropzone, ToolsPanel } from './ToolsChrome';
 
 function newId() {
 	return `${Date.now().toString(36)}-${Math.random().toString(36).slice(2, 8)}`;
