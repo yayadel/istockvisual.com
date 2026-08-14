37d499f74982a4faa2c32218e68da7f00a7f6914
yayadel <yayadel@users.noreply.github.com>
2026-08-14 18:22:31 +0800

    auto backup (cursor): 2026-08-14 18:22:30

--- src/components/AssetPreviewPanel.tsx
+++ src/components/AssetPreviewPanel.tsx
@@ -4,7 +4,7 @@ import ShareBar from './ShareBar';
 
 const LENS_SIZE = 196;
 const LENS_ZOOM = 2.5;
-const EDIT_HINT_SECONDS = 5;
+const EDIT_HINT_MS = 5000;
 const EDIT_HINT_SHATTER_MS = 620;
 
 type Props = {
